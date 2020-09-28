d9603f9ff1e6874829298eb9c6429ea51553650e
Malena Bengtsson <malena94bengtsson@gmail.com>
2020-09-28 08:56:43 +0200

    Fixed typo in import

--- Factory/UnitFactory.js
+++ Factory/UnitFactory.js
@@ -5,14 +5,14 @@ const Lich = require('../UnitStructure/UnitClasses/Lich')
 const Minotaur = require('../UnitStructure/UnitClasses/Minotaur')
 const Orc = require('../UnitStructure/UnitClasses/Orc')
 const Pixie = require('../UnitStructure/UnitClasses/Pixie')
-const Trogdolyte = require('../UnitStructure/UnitClasses/Trogdolyte')
+const Troglodyte = require('../UnitStructure/UnitClasses/Troglodyte')
 const Unicorn = require('../UnitStructure/UnitClasses/Unicorn')
 const Witch = require('../UnitStructure/UnitClasses/Witch')
 
 module.exports = class UnitFactory {
   
   generateUnits() {
-  
-
+    //give each creature its own number, and then randomize and push into array?
+    // Any other way to fix random units?
   }
 }
